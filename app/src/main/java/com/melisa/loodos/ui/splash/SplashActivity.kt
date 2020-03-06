718ca21dfd48020127f0d88188e2a81f212d919b
gokhandiker <gkhandiker@gmail.com>
2020-03-06 22:24:44 +0300

    firebase remote eklendi

--- app/src/main/java/com/melisa/loodos/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/melisa/loodos/ui/splash/SplashActivity.kt
@@ -1,37 +1,64 @@
 package com.melisa.loodos.ui.splash
 
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.melisa.loodos.R
-import com.melisa.loodos.ui.main.MainActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class SplashActivity : AppCompatActivity() {
     private val SPLASH_TIME_OUT:Long=3000 // 3 sec
+
+    private lateinit var firebaseRemoteConfig: FirebaseRemoteConfig
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+
+        firebaseRemoteConfig.setConfigSettings(
+            FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(true)
+                .build()
+        )
+
+        firebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
 
 
+        textView.setTextColor(Color.parseColor(firebaseRemoteConfig.getString("text_color")))
+        textView.textSize = firebaseRemoteConfig.getValue("text_size").asDouble().toFloat()
+        textView.text = firebaseRemoteConfig.getString("text_str")
     }
 
     override fun onStart() {
         super.onStart()
-        Handler().postDelayed({
-            // This method will be executed once the timer is over
-            // Start your app main activity
 
-            startActivity(Intent(this, MainActivity::class.java))
 
-            // close this activity
-            finish()
-        }, SPLASH_TIME_OUT)
+        /*
+                This will initiate fetching of parameters. We have set the expiry time as 0
+                which will ensure we get fresh parameters every time
+                */
+
+        /*
+                This will initiate fetching of parameters. We have set the expiry time as 0
+                which will ensure we get fresh parameters every time
+                */firebaseRemoteConfig.fetch(0)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Activated", Toast.LENGTH_SHORT).show()
+                    firebaseRemoteConfig.activateFetched()
+                } else {
+                    Toast.makeText(this, "Not Activated", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
     }
 
 
