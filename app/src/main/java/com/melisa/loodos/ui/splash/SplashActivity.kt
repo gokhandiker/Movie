fde1415c3d82ecf140f645e491a42894eff6a43a
gokhandiker <gkhandiker@gmail.com>
2020-03-06 18:23:22 +0300

    main and splash screen added

--- app/src/main/java/com/melisa/loodos/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/melisa/loodos/ui/splash/SplashActivity.kt
@@ -1,13 +1,16 @@
 package com.melisa.loodos.ui.splash
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.melisa.loodos.R
+import com.melisa.loodos.ui.main.MainActivity
 
 
 class SplashActivity : AppCompatActivity() {
-
+    private val SPLASH_TIME_OUT:Long=3000 // 3 sec
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +19,19 @@ class SplashActivity : AppCompatActivity() {
 
 
 
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 
 
